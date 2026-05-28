eaea417550fdd38e037bb84484e8bcb6dceb0854
atit1912 <167001916+atit1912@users.noreply.github.com>
2026-05-28 18:37:50 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// config.js — GAS Web App URL
-var GAS_URL = 'https://script.google.com/macros/s/AKfycbz-wQVWKdqxcfHY5nQVepzm9sfIFsING6g_qqXj0HMK5FctyQb5EHNGhNg9wdjFLLFh/exec';
+// config.js — GAS Web App URL (updated)
+var GAS_URL = 'https://script.google.com/macros/s/AKfycbz-wQVWKdqxcfHY5nQVepzm9sflFsING6g_qqXj0HMK5FctyQb5EHNGhNg9wdjFLLFh/exec';

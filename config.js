a3af9307d914ace157e39db18885f00b862cd15a
atit1912 <167001916+atit1912@users.noreply.github.com>
2026-05-31 19:42:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js — GAS Web App URL (updated)
-var GAS_URL = 'https://script.google.com/macros/s/AKfycbwcIgP372M5g9AmR8LRMg0HvnzSGNOCMKe4ejnAVvU7qVSyPHo5Yhu1R5zCgU_0FZE/exec';
+var GAS_URL = 'https://script.google.com/macros/s/AKfycbwL56_UEGe61_EZyJB3NfiXqHS87GxyYQ9eO3YZxT56CbMOma75JqOmAr4LpsbOqVg/exec';
